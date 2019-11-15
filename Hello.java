70db66342d635dc47c467a83236594bad3defdd5
Taylor Palm <taylor.palm@yahoo.com>
2019-11-15 10:25:30 -0500

    Oops, we didn't want this comment

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String argv[])
 	{
 
-		//Default is "World"
+		//This is an unwanted but staged comment
 
 		String name= "World";
 		if(argv.length!= 0){
